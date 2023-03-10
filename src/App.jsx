6bb081c6e588a47939fe6dfad058a03bd96cee7e
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2023-03-09 23:32:31 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import './scss/styles.scss'
+import { Home } from './components/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <Home />
+      </div>
+  )
+}
+
+export default App
